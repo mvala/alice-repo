88eb4a22bdef075f66ac114f9b7216e95f05a08d
Martin Vala <Martin.Vala@cern.ch>
2013-12-08 12:40:06 +0100

    Fix for pullAliroot from git

--- utils/pull-aliroot.sh
+++ utils/pull-aliroot.sh
@@ -8,15 +8,26 @@ fi
 
 MY_VER=${1//-/.}
 
-SVN_PATH1="AliRoot_svn_$1"
-SVN_PATH2="alice-aliroot-an-$MY_VER"
-WC_FILE="wc.db"
+if [ ! -d $HOME/ALICE/AliRoot ];then
+    mkdir -p $HOME/ALICE/
+    cd $HOME/ALICE/
+    git clone git clone http://git.cern.ch/pub/AliRoot
+else 
+    git checkout master
+    git pull
+fi
 
-#svn co https://alisoft.cern.ch/AliRoot/tags/v$1-AN $SVN_PATH1
-svn co http://svn.cern.ch/guest/AliRoot/tags/v$1-AN $SVN_PATH1
-cp $SVN_PATH1/.svn/$WC_FILE $WC_FILE
-svn -q export $SVN_PATH1 $SVN_PATH2
-mkdir $SVN_PATH2/.svn/
-mv $WC_FILE $SVN_PATH2/.svn/
-tar cfz $SVN_PATH2.tar.gz $SVN_PATH2
-rm -rf $SVN_PATH2 $SVN_PATH1
+git archive --format=tar.gz --prefix=alice-aliroot-an-$MY_VER/ v$1-AN > $HOME/rpmbuild/SOURCES/alice-aliroot-an-$MY_VER.tar.gz
+
+#SVN_PATH1="AliRoot_svn_$1"
+#SVN_PATH2="alice-aliroot-an-$MY_VER"
+#WC_FILE="wc.db"
+
+##svn co https://alisoft.cern.ch/AliRoot/tags/v$1-AN $SVN_PATH1
+#svn co http://svn.cern.ch/guest/AliRoot/tags/v$1-AN $SVN_PATH1
+#cp $SVN_PATH1/.svn/$WC_FILE $WC_FILE
+#svn -q export $SVN_PATH1 $SVN_PATH2
+#mkdir $SVN_PATH2/.svn/
+#mv $WC_FILE $SVN_PATH2/.svn/
+#tar cfz $SVN_PATH2.tar.gz $SVN_PATH2
+#rm -rf $SVN_PATH2 $SVN_PATH1
